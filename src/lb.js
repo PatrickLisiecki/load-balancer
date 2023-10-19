660f0ab8f508c90b17d7f5b630d166099f4d730f
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-10-18 23:51:17 -0400

    Set up load balancer to listen for requests

--- src/lb.js
+++ src/lb.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Server is up and running.");
+  console.log(req);
+  res.send("Load balancer is up and running.");
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on ${port}...`);
+  console.log(`Load balancer is listening on port ${port}...`);
 });
